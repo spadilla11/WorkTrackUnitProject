6248f0d56fa132a64a21ba0405b6b8b61469bfcc
spadilla11 <dpadilla26@basecampcodingacademy.org>
2026-03-11 10:48:35 -0500

    Initial Commit

---
+++ src/main/java/org/example/worktrack/WorkTrackApplication.java
@@ -0,0 +1,13 @@
+package org.example.worktrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkTrackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkTrackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/worktrack/WorkTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.worktrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkTrackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
